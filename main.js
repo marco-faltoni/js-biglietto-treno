3d9fce4611d5913ff035e909a89e21a655e0a6b1
Marco <marco.faltoni@outlook.it>
2020-04-20 15:34:03 +0200

    piccole correzioni, esercizio fatto

--- main.js
+++ main.js
@@ -12,23 +12,25 @@ console.log(eta);
 var prezzo_totale = (chilometri * 0.21);
 console.log(prezzo_totale);
 
+var prezzo_over = '-';
 
+var prezzo_minorenni = '-';
 
 // imposto sconto del 20% per minorenni e 40% per over 65, e visualizzo risultato
 // procedimento A
 if(eta >= 18 && eta <= 65) {
     // solo se hai più di 18 anni e meno di 65 anni prezzo pieno
-    var prezzo_totale = (chilometri * 0.21) + '€';
+    prezzo_totale = (chilometri * 0.21);
     console.log(prezzo_totale);
 } else if(eta < 18) {
     // la prima if è false
     // l'età è < 18
-    var prezzo_minorenni = ((prezzo_totale - (prezzo_totale * 20) / 100))  + '€';
+    prezzo_minorenni = ((prezzo_totale - (prezzo_totale * 20) / 100));
     console.log(prezzo_minorenni);
 } else {
     // la prima if è falsa ed è falsificata dalla seconda condizione
     // caso in cui l'età è > 65
-    var prezzo_over = ((prezzo_totale - (prezzo_totale * 40) / 100))  + '€';
+    prezzo_over = ((prezzo_totale - (prezzo_totale * 40) / 100));
     console.log(prezzo_over);
 }
 
